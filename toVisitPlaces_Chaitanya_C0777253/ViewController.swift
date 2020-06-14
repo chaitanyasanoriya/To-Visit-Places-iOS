7d35dcf1be05606238af405b07aa86ae2ec7ae92
chetu97 <chaitanya.sanoriya@gmail.com>
2020-06-13 20:42:30 -0400

    Initial Commit

---
+++ toVisitPlaces_Chaitanya_C0777253/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  toVisitPlaces_Chaitanya_C0777253
+//
+//  Created by Chaitanya Sanoriya on 13/06/20.
+//  Copyright © 2020 Chaitanya Sanoriya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
